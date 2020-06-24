796a6ce1c7ebb050f62c592eec37e3543bcb8f56
Felipe Kosouski <felipe.kosouski@gmail.com>
2020-06-24 10:00:31 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Router from 'express'
+import UsersController from './controllers/UsersController'
+
+const routes = Router()
+
+routes.get('/users', UsersController.index)
+routes.post('/users', UsersController.store)
+
+export default routes
